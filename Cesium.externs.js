51c03e0ce738589c6d5fea7d23a48bab04f91048
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2014-10-22 14:41:21 +0200

    Add IntersectionTests.rayEllipsoid and Ray.getPoint Cesium externs.

--- Cesium.externs.js
+++ Cesium.externs.js
@@ -1649,7 +1649,7 @@ Cesium.Ellipsoid = function(radii) {};
 
 
 /**
- * @type {Cesium.Ellipsoid}
+ * @type {!Cesium.Ellipsoid}
  */
 Cesium.Ellipsoid.WGS84;
 
@@ -2406,3 +2406,28 @@ Cesium.BingMapsStyle.AERIAL_WITH_LABELS;
 
 /** @type {!Cesium.BingMapsStyle} */
 Cesium.BingMapsStyle.ROAD;
+
+/** @constructor */
+Cesium.IntersectionTests = function() {};
+
+/**
+ * @typedef {{
+ *   start: number,
+ *   end: number
+ * }}
+ */
+Cesium.StartEndObject;
+
+/**
+ * @param {!Cesium.Ray} ray
+ * @param {!Cesium.Ellipsoid} ellipsoid
+ * @return {Cesium.StartEndObject}
+ */
+Cesium.IntersectionTests.rayEllipsoid = function(ray, ellipsoid) {};
+
+/**
+ * @param {!Cesium.Ray} ray
+ * @param {number} distance
+ * @return {!Cesium.Cartesian3}
+ */
+Cesium.Ray.getPoint = function(ray, distance) {};
