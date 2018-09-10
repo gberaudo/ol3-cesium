c9da2794bdb7e557bccff22634888d4c0813dde5
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2018-09-10 17:51:25 +0200

    Add Ga symbols to index.library.js

--- src/index.library.js
+++ src/index.library.js
@@ -1,5 +1,14 @@
 import OLCesium from './olcs/OLCesium.js';
+import GaKmlSynchronizer from './olcs/GaKmlSynchronizer.js';
+import GaRasterSynchronizer from './olcs/GaRasterSynchronizer.js';
+import GaTileset3dSynchronizer from './olcs/GaTileset3dSynchronizer.js';
+import GaVectorSynchronizer from './olcs/GaVectorSynchronizer.js';
+
 export default OLCesium;
 
 const olcs = window['olcs'] = {};
 olcs.OLCesium = OLCesium;
+olcs.GaKmlSynchronizer = GaKmlSynchronizer;
+olcs.GaRasterSynchronizer = GaRasterSynchronizer;
+olcs.GaTileset3dSynchronizer = GaTileset3dSynchronizer;
+olcs.GaVectorSynchronizer = GaVectorSynchronizer;
